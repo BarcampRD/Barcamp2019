8db385b1013b37185d4cc8376fe2f93e33e1a0b0
Felix Alejandro Guzman <felixalejandro@outlook.com>
2019-11-14 01:30:41 -0400

    Mostrando biografia de charlistas

--- js/charlistas.js
+++ js/charlistas.js
@@ -17,10 +17,18 @@ function charlistas() {
                 speakers.innerHTML += createCharlistaCard(json[i], i);
             }
 
-            $("#biography").click(function () {
-                $(".modal-biography").addClass("open");
+    
+
+            $(".more").click(function () {
+
+                var element = $(this);
+
+                mostrarModal(element.attr('id'));
+
+            //     // $(".modal-biography").addClass("open");
                 $(".hide-modal").click(function () {
                     $(".modal-biography").removeClass("open");
+                    return false;
                 });
             });
         
@@ -30,7 +38,6 @@ function charlistas() {
 
 function createCharlistaCard(charlistaJson, index) {
 
-    console.log('ruta: ' + charlistaJson.imagen);
 
     var speakerHtml = "<div class=\"col-xl-3 col-lg-4 col-md-6 col-sm-6 col-xs-8\">" +
         "<div class=\"charlista\" style='background-image:url(" +  charlistaJson.imagen + ");'>" +
@@ -45,12 +52,27 @@ function createCharlistaCard(charlistaJson, index) {
 
     speakerHtml += "</div>" ;
 
-    speakerHtml += '<a href="#" class="more" id="biography">Ver Biografia<span class="icon-arrow-right"></span></a>';
+    speakerHtml += '<a class="more" id="biography_'+index+'">Ver Biografia<span class="icon-arrow-right"></span></a>';
 
+    speakerHtml +=  '<div class="modal-biography"  id="modal_biography_'+index+'">     <div class="modal-content">         <div class="modal-header"><div class="row">                 <div class="col-lg-5 col-md-6 col-sm-6">  <img src="'+charlistaJson.imagen+'" class="img-fluid"></div><div class="col-lg-6 col-md-6 col-sm-6 pt-4"><h5>'+ charlistaJson.nombre +'</h5><div class="social">';
+  
+    for (i in charlistaJson.socials) {
+        speakerHtml += '<a href="' + charlistaJson.socials[i].link + '" target="_blank"> <span><i class="' + charlistaJson.socials[i].icon + '"></i></span></a> ';
+    }
 
+    speakerHtml += '</div></div></div></div><div class="modal-body"><p>'+charlistaJson.bio+'</p><a href="#" class="hide-modal"><span class="icon-arrow-left"></span>Ocultar Biografia</a></div></div></div>';
     speakerHtml +="</div>" +
         "</div>" +
         "</div>";
 
     return speakerHtml;
+}
+
+function mostrarModal(id) {
+    
+
+    
+    $("#modal_"+id).addClass("open");
+
+    return false;
 }
